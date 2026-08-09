2482b37971e0c44edbc2601e38e9a13574aefaad
Shubham Dhopat <shubhamdhopat@Shubhams-MacBook-Air.local>
2026-08-10 01:02:06 +0530

    fixed config issue

--- src/my-trips/index.jsx
+++ src/my-trips/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "@/service/FirebaseConfig";
+import { db } from "@/service/firebaseConfig";
 import UserTripCardItem from "./components/UserTripCardItem";
 
 function MyTrips() {
